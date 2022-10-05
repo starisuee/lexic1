0afaf00e06bbee5143cb07b362553521b2611a62
Eduardo Phillips <starisuee@outlook.com>
2022-10-05 16:36:07 -0400

    tt

---
+++ script.bat
@@ -0,0 +1,13 @@
+@echo off
+C:\Users\Gabriel\Desktop\lexico1.l
+echo "FLEX COMPLILO LA ESPECIFICACION LEXICA DEL ARCHIVO LEXICO 1 (SE CREA ARCHIVO lex.yy.c)"
+pause
+C:\Users\Gabriel\Desktop\ //ruta lexico
+echo "finaliza la ejecucion del compilador c sobre el archivo lex.yy.c c o m p i l a d o . e x e"
+pause
+Compilado.exe Prueba.txt
+echo "se ejecuta el analizador lexico sobre el archivo"
+del lex.yy.c
+del Compilado.exe
+echo "se eliminaron archivos, finalizar"
+pause
